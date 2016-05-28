7404c555cb61329739be4a50992af325411d2c67
MiriamMarieTy <mariethegreat89303@gmail.com>
2016-05-29 01:41:21 +0800

    added view for login

--- saisX/enrollment/views.py
+++ saisX/enrollment/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.contrib.auth import login
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView, FormView
 from .models import Student
+from .forms import UserLoginForm
 
-class HomeView (TemplateView):
+class HomeView(TemplateView):
     model = Student
     template_name = 'enrollment/home.html'
+
+
+class LoginView(FormView):
+    template_name = "enrollment/login.html"
+    form_class = UserLoginForm
+    success_view_name = 'enrollment:home'
+
+    def get(self, request, *args, **kwargs):
+        return super(LoginView, self).get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        user = form.cleaned_data['user']
+        login(self.request, user)
+
+        return super(LoginView, self).form_valid(form)
+
+    def get_success_url(self):
+        next = self.request.GET.get('next')
+        if next:
+            return next
+
+        else:
+            return reverse(self.success_view_name)
