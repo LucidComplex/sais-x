e2cdefcdd466c532a0638ebb8057f9cbfc714ec6
MiriamMarieTy <mariethegreat89303@gmail.com>
2016-05-29 00:48:04 +0800

    Fixed typos, and incorrect codes (codes with red lines)

--- saisX/enrollment/views.py
+++ saisX/enrollment/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.view.generic import TemplateView, ListVew
-from enrollment.views import Student
+from django.views.generic import TemplateView, ListView
+from .models import Student
 
-class HomeView (TemplateView)
+class HomeView (TemplateView):
     model = Student
     template = 'enrollment/home.html'
