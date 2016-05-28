c91c7c213886633cedb79ac74f9db1119eda6bf4
MiriamMarieTy <mariethegreat89303@gmail.com>
2016-05-28 16:59:00 +0800

    Registered models

--- saisX/enrollment/admin.py
+++ saisX/enrollment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Address)
+admin.site.register(models.Course)
+admin.site.register(models.Institution)
+admin.site.register(models.Program)
+admin.site.register(models.Student)
