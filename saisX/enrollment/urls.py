5ccbcc2b4b2e72c8c4828092bd58ba311fc8e083
Paks <vincentpakson@gmail.com>
2016-05-29 17:45:46 +0800

    reverted commits

--- saisX/enrollment/urls.py
+++ saisX/enrollment/urls.py
@@ -7,5 +7,6 @@ admin.autodiscover()
 urlpatterns = [
     url(r'home$', views.HomeView.as_view(), name='home'),
     url(r'login$', views.LoginView.as_view(), name="login"),
-    url(r'profile$', views.ProfileView.as_view(), name='profile')
+    url(r'profile$', views.ProfileView.as_view(), name='profile'),
+    url(r'grades$', views.GradesView.as_view(), name='grades'),
 ]
